c0ab3e02791141919533b6567b1c2637bf02d391
K. Alex Mills <k.alex.mills@gmail.com>
2023-04-28 22:01:11 -0500

    refactor/doc: make till.go

--- internal/main_scene.go
+++ internal/main_scene.go
@@ -164,6 +164,7 @@ func (m *MainScene) Draw(screen *ebiten.Image) {
 	// draw counter
 	m.counter.DrawTo(screen)
 
+	// draw all the sprites in their draw order.
 	for _, sprite := range m.Sprites {
 		sprite.DrawTo(screen)
 	}
@@ -182,7 +183,7 @@ type Money struct {
 	IsCoin bool
 }
 
-// newBill creates in local coordinates on the counter.
+// newBill creates a bill of the provided denomination in local coordinates on the counter.
 func newBill(denom int, x, y int) Sprite {
 	x = clamp(x+112, 112, 320-43)
 	y = clamp(y+152, 152, 240-43)
@@ -199,7 +200,7 @@ func newBill(denom int, x, y int) Sprite {
 	}
 }
 
-// newCoin creates in local coordinates on the counter.
+// newCoin creates a coin of the provided denomination in local coordinates on the counter.
 func newCoin(denom int, x, y int) Sprite {
 	x = clamp(x+112, 112, 320-15)
 	y = clamp(y+152, 152, 240-15)
@@ -215,6 +216,7 @@ func newCoin(denom int, x, y int) Sprite {
 	}
 }
 
+// clampToCounter clamps the provided point to the counter range (hardcoded)
 func clampToCounter(pt image.Point) image.Point {
 	pt.X = clamp(pt.X, 112, 320-15)
 	pt.Y = clamp(pt.Y, 152, 240-15)
@@ -234,100 +236,6 @@ func clamp(x int, min, max int) int {
 func cursorPos() image.Point {
 	mx, my := ebiten.CursorPosition()
 	return image.Pt(mx/2, my/2)
-}
-
-const CoinTargets = 0
-const BillTargets = 1
-
-type Till struct {
-	*BaseSprite
-
-	DropTargets [2][5]image.Rectangle
-	BillSlots   [5][]*Money
-	CoinSlots   [5][]*Money
-}
-
-func NewTill() *Till {
-	// TODO: generate random bills in the Till
-	return &Till{
-		BaseSprite: &BaseSprite{
-			X: 0, Y: 172,
-			Img: Resources.images["Till"],
-		},
-		DropTargets: [2][5]image.Rectangle{
-			CoinTargets: { //
-				rect(4, 50, 20, 15),
-				rect(25, 50, 20, 15),
-				rect(46, 50, 20, 15),
-				rect(67, 50, 20, 15),
-				rect(88, 50, 20, 15),
-			},
-			BillTargets: { // bill targets
-				rect(4, 3, 20, 45),
-				rect(25, 3, 20, 45),
-				rect(46, 3, 20, 45),
-				rect(67, 3, 20, 45),
-				rect(88, 3, 20, 45),
-			},
-		},
-	}
-}
-
-// Drop drops the provided sprite on the Till; landing it in the location needed.
-func (t *Till) Drop(s Sprite) bool {
-	m, ok := s.(*Money)
-	if !ok {
-		fmt.Println("not money!")
-		return false
-	}
-	var targets [5]image.Rectangle
-	if m.IsCoin {
-		targets = t.DropTargets[CoinTargets]
-	} else {
-		targets = t.DropTargets[BillTargets]
-	}
-	fmt.Println("targets", targets)
-	// find drop target with max area intersection
-	bestIdx, maxA := -1, 0
-	for idx, rect := range targets {
-		sz := m.Bounds().Intersect(rect.Add(t.Pos())).Size()
-		fmt.Println("rect:", rect, "sz:", sz, "pos:", t.Pos())
-		a := sz.X * sz.Y
-		if a > 0 && a > maxA {
-			bestIdx = idx
-			maxA = a
-		}
-	}
-	if bestIdx == -1 {
-		return false
-	}
-	r := targets[bestIdx].Add(t.Pos())
-	m.ClampToRect(r)
-	if m.IsCoin {
-		t.CoinSlots[bestIdx] = append(t.CoinSlots[bestIdx], m)
-	} else {
-		t.BillSlots[bestIdx] = append(t.BillSlots[bestIdx], m)
-	}
-	return true
-}
-
-// Remove removes the provided money from the Till; checking the top of each stack of bills and coins for it.
-func (t *Till) Remove(s Sprite) {
-	m, ok := s.(*Money)
-	if !ok {
-		return
-	}
-	for i := 0; i < 5; i++ {
-		if len(t.BillSlots[i]) > 0 && t.BillSlots[i][len(t.BillSlots[i])-1] == m {
-			fmt.Println("removed", m.Value, "from", i)
-			t.BillSlots[i] = t.BillSlots[i][:len(t.BillSlots[i])-1]
-			return
-		}
-		if len(t.CoinSlots[i]) > 0 && t.CoinSlots[i][len(t.CoinSlots[i])-1] == m {
-			t.CoinSlots[i] = t.CoinSlots[i][:len(t.CoinSlots[i])-1]
-			return
-		}
-	}
 }
 
 func rect(x, y, w, h int) image.Rectangle {
---
+++ internal/till.go
@@ -0,0 +1,100 @@
+package internal
+
+import (
+	"fmt"
+	"image"
+)
+
+const CoinTargets = 0
+const BillTargets = 1
+
+type Till struct {
+	*BaseSprite
+
+	DropTargets [2][5]image.Rectangle
+	BillSlots   [5][]*Money
+	CoinSlots   [5][]*Money
+}
+
+func NewTill() *Till {
+	// TODO: generate random bills in the Till
+	return &Till{
+		BaseSprite: &BaseSprite{
+			X: 0, Y: 172,
+			Img: Resources.images["Till"],
+		},
+		DropTargets: [2][5]image.Rectangle{
+			CoinTargets: { //
+				rect(4, 50, 20, 15),
+				rect(25, 50, 20, 15),
+				rect(46, 50, 20, 15),
+				rect(67, 50, 20, 15),
+				rect(88, 50, 20, 15),
+			},
+			BillTargets: { // bill targets
+				rect(4, 3, 20, 45),
+				rect(25, 3, 20, 45),
+				rect(46, 3, 20, 45),
+				rect(67, 3, 20, 45),
+				rect(88, 3, 20, 45),
+			},
+		},
+	}
+}
+
+// Drop drops the provided sprite on the Till; landing it in the location needed.
+func (t *Till) Drop(s Sprite) bool {
+	m, ok := s.(*Money)
+	if !ok {
+		fmt.Println("not money!")
+		return false
+	}
+	var targets [5]image.Rectangle
+	if m.IsCoin {
+		targets = t.DropTargets[CoinTargets]
+	} else {
+		targets = t.DropTargets[BillTargets]
+	}
+	fmt.Println("targets", targets)
+	// find drop target with max area intersection
+	bestIdx, maxA := -1, 0
+	for idx, rect := range targets {
+		sz := m.Bounds().Intersect(rect.Add(t.Pos())).Size()
+		fmt.Println("rect:", rect, "sz:", sz, "pos:", t.Pos())
+		a := sz.X * sz.Y
+		if a > 0 && a > maxA {
+			bestIdx = idx
+			maxA = a
+		}
+	}
+	if bestIdx == -1 {
+		return false
+	}
+	r := targets[bestIdx].Add(t.Pos())
+	m.ClampToRect(r)
+	if m.IsCoin {
+		t.CoinSlots[bestIdx] = append(t.CoinSlots[bestIdx], m)
+	} else {
+		t.BillSlots[bestIdx] = append(t.BillSlots[bestIdx], m)
+	}
+	return true
+}
+
+// Remove removes the provided money from the Till; checking the top of each stack of bills and coins for it.
+func (t *Till) Remove(s Sprite) {
+	m, ok := s.(*Money)
+	if !ok {
+		return
+	}
+	for i := 0; i < 5; i++ {
+		if len(t.BillSlots[i]) > 0 && t.BillSlots[i][len(t.BillSlots[i])-1] == m {
+			fmt.Println("removed", m.Value, "from", i)
+			t.BillSlots[i] = t.BillSlots[i][:len(t.BillSlots[i])-1]
+			return
+		}
+		if len(t.CoinSlots[i]) > 0 && t.CoinSlots[i][len(t.CoinSlots[i])-1] == m {
+			t.CoinSlots[i] = t.CoinSlots[i][:len(t.CoinSlots[i])-1]
+			return
+		}
+	}
+}
